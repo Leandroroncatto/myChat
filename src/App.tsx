8c0ce89e9e56faf2d2493f9bfd0197f4bbe95ad6
Leandro <leandroroncatto18@gmail.com>
2025-11-13 15:26:32 -0300

    refactor: replace ChatsPage route with TabLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import ChatsPage from "./pages/chats/ChatsPage";
+import TabLayout from "./pages/TabLayout";
 
 function App() {
     return (
@@ -9,7 +9,7 @@ function App() {
                     <Route path="/auth/register" element={""}></Route>
                     <Route path="/auth/login" element={""}></Route>
 
-                    <Route path="/" element={<ChatsPage />}></Route>
+                    <Route path="/" element={<TabLayout />}></Route>
                     <Route path="/chat/:id" element={""}></Route>
                     <Route path="/settings" element={""}></Route>
                     <Route path="/profile" element={""}></Route>
