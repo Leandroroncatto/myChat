9c726eb2a5925bc0fb8e68ce037508b24fa67582
Leandro <leandroroncatto18@gmail.com>
2025-11-13 15:29:21 -0300

    styles: adjust light/dark theme colors

--- src/components/ui/EmptyChat.tsx
+++ src/components/ui/EmptyChat.tsx
@@ -2,15 +2,14 @@ import { CloudFogIcon } from "lucide-react";
 
 export default function EmptyChat() {
     return (
-        <>
-            <div className="flex items-center justify-center h-screen">
-                <div className="flex flex-col items-center justify-center">
-                    <CloudFogIcon size={120} className="text-blue-500" />
-                    <h4 className="font-semibold">
-                        Select a conversation or start a <span className="font-bold text-blue-500 underline cursor-pointer">new one</span>
-                    </h4>
-                </div>
+        <div className="flex items-center justify-center w-full h-full">
+            <div className="flex flex-col items-center justify-center text-center">
+                <CloudFogIcon size={120} className="mb-4 text-blue-500" />
+                <h4 className="font-semibold text-gray-800 dark:text-slate-200">
+                    Select a conversation or start a{" "}
+                    <span className="font-bold text-blue-500 underline cursor-pointer">new one</span>
+                </h4>
             </div>
-        </>
+        </div>
     );
 }
