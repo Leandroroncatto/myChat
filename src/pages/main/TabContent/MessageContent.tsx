d204d527f6a9a711f2fe7e4695e7020088269c78
Leandro <leandroroncatto18@gmail.com>
2025-11-14 16:38:43 -0300

    feat: improve tabs behavior when there is an active chat

--- src/pages/main/TabContent/MessageContent.tsx
+++ src/pages/main/TabContent/MessageContent.tsx
@@ -1,16 +1,25 @@
+import { useParams } from "react-router-dom";
 import MessagePanel from "../../../components/chats/messageTab/MessageTab";
 import ChatWindow from "../../../components/ui/ChatWindow";
 
 export default function MessageContent() {
+    const { id: activeChat } = useParams();
+
     return (
         <>
-            <div className="flex-1 lg:hidden">
-                <MessagePanel />
-            </div>
+            {!activeChat ? (
+                <div className="flex-1 lg:hidden">
+                    <MessagePanel />
+                </div>
+            ) : (
+                <div className="flex-1 lg:hidden">
+                    <ChatWindow activeChat={activeChat} />
+                </div>
+            )}
 
             <div className="hidden w-full h-full lg:flex lg:flex-row">
                 <MessagePanel />
-                <ChatWindow />
+                <ChatWindow activeChat={activeChat} />
             </div>
         </>
     );
--- src/pages/main/TabContent/SettingsContent.tsx
+++ src/pages/main/TabContent/SettingsContent.tsx
@@ -1,16 +1,39 @@
+import { useParams } from "react-router";
 import SettingsTab from "../../../components/chats/settingsTab/SettingsTab";
 import ChatWindow from "../../../components/ui/ChatWindow";
+import { useEffect, useState } from "react";
 
 export default function SettingsContent() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 1024);
+    const { id: activeChat } = useParams();
+
+    useEffect(() => {
+        function onResize() {
+            setIsMobile(window.innerWidth < 1024);
+        }
+        window.addEventListener("resize", onResize);
+        return () => window.removeEventListener("resize", onResize);
+    }, []);
+
     return (
         <>
-            <div className="flex-1 lg:hidden">
-                <SettingsTab />
-            </div>
+            {!activeChat ? (
+                <div className="flex-1 lg:hidden">
+                    <SettingsTab />
+                </div>
+            ) : isMobile ? (
+                <div className="flex-1 lg:hidden">
+                    <SettingsTab />
+                </div>
+            ) : (
+                <div className="flex-1 lg:hidden">
+                    <ChatWindow activeChat={activeChat} />
+                </div>
+            )}
 
             <div className="hidden w-full h-full lg:flex lg:flex-row">
                 <SettingsTab />
-                <ChatWindow />
+                <ChatWindow activeChat={activeChat} />
             </div>
         </>
     );
