5784919d17d97986cdf34511d7dd33a9403dd57a
Leandro <leandroroncatto18@gmail.com>
2025-11-14 18:57:44 -0300

    feat: add chat page

--- src/components/chat/ChatWindow.tsx
+++ src/components/chat/ChatWindow.tsx
@@ -1,10 +1,15 @@
+import ChatHeader from "./ChatHeader";
+import ChatBody from "./ChatBody";
 import EmptyChat from "./EmptyChat";
 
 export default function ChatWindow({ activeChat }: any) {
     return (
         <>
             {activeChat ? (
-                <div></div>
+                <div className="flex flex-col flex-1 h-full">
+                    <ChatHeader />
+                    <ChatBody />
+                </div>
             ) : (
                 <div className="hidden w-full min-h-full lg:block">
                     <EmptyChat />
