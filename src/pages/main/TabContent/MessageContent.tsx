a99b54dd92200fbde310ee5427ebbe902e250522
Leandro <leandroroncatto18@gmail.com>
2025-11-14 18:58:53 -0300

    styles: improve dark mode & responsivity

--- src/pages/main/TabContent/MessageContent.tsx
+++ src/pages/main/TabContent/MessageContent.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import MessagePanel from "../../../components/tabs/messageTab/MessageTab";
-import ChatWindow from "../../../components/ui/ChatWindow";
+import ChatWindow from "../../../components/chat/ChatWindow";
 
 export default function MessageContent() {
     const { id: activeChat } = useParams();
@@ -12,12 +12,12 @@ export default function MessageContent() {
                     <MessagePanel />
                 </div>
             ) : (
-                <div className="flex-1 lg:hidden">
+                <div className="flex-1 w-full h-full lg:hidden sm:pl-32 dark:bg-slate-800/30">
                     <ChatWindow activeChat={activeChat} />
                 </div>
             )}
 
-            <div className="hidden w-full h-full lg:flex lg:flex-row">
+            <div className="hidden w-full h-full lg:flex lg:flex-row dark:bg-slate-800/30">
                 <MessagePanel />
                 <ChatWindow activeChat={activeChat} />
             </div>
