d2e86ad28110dfc88472aa70cf8acf973274a7c0
Leandro <leandroroncatto18@gmail.com>
2025-11-20 13:17:39 -0300

    feat: create contact mock data

---
+++ src/data/mockContacts.ts
@@ -0,0 +1,5 @@
+export const contacts = [
+    { name: "Pessoa legal", status: "Online" },
+    { name: "Amigo de Trabalho", status: "Visto há 5min" },
+    { name: "Usuário X", status: "Offline" },
+];
