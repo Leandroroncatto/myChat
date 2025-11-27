18e8903fbae0d29046a62ae3826db49a29d7f822
Leandro <leandroroncatto18@gmail.com>
2025-11-27 16:06:22 -0300

    feat: implement languages support

--- src/components/tabs/messageTab/MessageSection.tsx
+++ src/components/tabs/messageTab/MessageSection.tsx
@@ -1,36 +1,48 @@
 import { Box, Search } from "lucide-react";
 import ChatItem from "./ChatItem";
+import { useContext } from "react";
+import LanguageContext from "../../../context/LanguageContext";
 
 export default function MessageSection() {
+    const { languageData } = useContext(LanguageContext);
+
     return (
         <>
             <div className="w-full h-screen py-8 sm:pl-32 lg:max-w-[600px] bg-white overflow-y-scroll border-r border-gray-300 dark:bg-slate-900 dark:border-slate-800 transition-colors duration-300">
                 <div className="px-4 pb-4 sm:px-1">
-                    <h1 className="text-3xl font-bold text-gray-900 dark:text-slate-100">Chats</h1>
+                    <h1 className="text-3xl font-bold text-gray-900 dark:text-slate-100">
+                        {languageData.messageSection?.title}
+                    </h1>
                 </div>
                 <div className="px-4 sm:px-1">
                     <div className="relative">
                         <input
                             type="text"
                             className="w-full py-2.5 pl-12 pr-4 text-gray-800 placeholder-gray-500 transition-all duration-200 bg-gray-100 border border-gray-300 rounded-2xl focus:border-blue-500 focus:ring-2 focus:ring-blue-500/30 focus:outline-none dark:text-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:placeholder-slate-500"
-                            placeholder="Search"
+                            placeholder={languageData.messageSection?.inputPlaceholder}
                         />
                         <Search className="absolute text-blue-500 top-[13px] left-4 dark:text-blue-400" size={20} />
                     </div>
                     <div className="pt-10 pb-3 border-b border-gray-300 dark:border-gray-800">
                         <div className="flex items-center justify-start gap-4 dark:text-blue-500">
                             <Box size={20} />
-                            <h2 className="text-sm font-semibold text-blue-500 dark:text-slate-100">Archived</h2>
+                            <h2 className="text-sm font-semibold text-blue-500 dark:text-slate-100">
+                                {languageData.messageSection?.archivedButton}
+                            </h2>
                         </div>
                     </div>
                     <div className="py-8">
-                        <h2 className="pb-4 text-base font-semibold text-gray-700 dark:text-slate-300">Pinned</h2>
+                        <h2 className="pb-4 text-base font-semibold text-gray-700 dark:text-slate-300">
+                            {languageData.messageSection?.pinnedTitle}
+                        </h2>
                         <ul className="flex flex-col gap-4">
                             <ChatItem />
                         </ul>
                     </div>
                     <div>
-                        <h2 className="pb-4 text-base font-semibold text-gray-700 dark:text-slate-300">All chats</h2>
+                        <h2 className="pb-4 text-base font-semibold text-gray-700 dark:text-slate-300">
+                            {languageData.messageSection?.allChatsTitle}
+                        </h2>
                         <ul className="flex flex-col gap-4 pb-12 sm:pb-0">
                             <ChatItem />
                             <ChatItem />
