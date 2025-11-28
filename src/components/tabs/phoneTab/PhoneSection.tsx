362b94d72cec63c295c8b2055d2ec8f26dc3d774
Leandro <leandroroncatto18@gmail.com>
2025-11-28 16:47:23 -0300

    feat: add language context translations

--- src/components/tabs/phoneTab/PhoneSection.tsx
+++ src/components/tabs/phoneTab/PhoneSection.tsx
@@ -4,20 +4,17 @@ import { AlertTriangle } from "lucide-react";
 
 export default function PhoneSection() {
     const { languageData } = useContext(LanguageContext);
-    console.log(languageData);
-
-    const warningTitle = "Funcionalidade Indisponível";
-    const warningMessage =
-        "Esta seção não é uma funcionalidade ativa no momento. Por favor, volte mais tarde para ver as novidades!";
 
     return (
         <>
             <div className="w-full h-screen py-8 sm:pl-32 lg:max-w-[600px] bg-white overflow-y-scroll border-r border-gray-300 dark:bg-slate-900 dark:border-slate-800 transition-colors duration-300">
                 <div className="flex flex-col gap-4 items-center justify-center w-full h-full">
                     <AlertTriangle className="text-blue-500" size={72} />
-                    <h1 className="text-2xl font-bold text-gray-800 dark:text-slate-200">{warningTitle}</h1>
+                    <h1 className="text-2xl font-bold text-gray-800 dark:text-slate-200">
+                        {languageData.phoneSection.warningTitle}
+                    </h1>
                     <p className="text-sm text-center max-w-[300px] text-gray-700 dark:text-slate-300">
-                        {warningMessage}
+                        {languageData.phoneSection.warningMessage}
                     </p>
                 </div>
             </div>
