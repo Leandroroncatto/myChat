fabae7966ee8224f6fbc8faf9527a30d9b281baf
Leandro <leandroroncatto18@gmail.com>
2025-11-28 19:07:15 -0300

    feat: implement profile dropdown & search bar

--- src/components/chat/ChatHeader.tsx
+++ src/components/chat/ChatHeader.tsx
@@ -1,23 +1,51 @@
-import { Phone, Settings } from "lucide-react";
+import { Phone, Search, Video } from "lucide-react";
 import testProfilePicture from "../../assets/testProfilePicture.jpg";
+import { useRef, useState } from "react";
+import ProfileDropdown from "./ProfileDropDown";
+import SearchBar from "./SearchBar";
 
 export default function ChatHeader() {
+    const [activeDropdown, setActiveDropdown] = useState(false);
+    const [activeSearchBar, setActiveSearchBar] = useState(false);
+    const triggerRef = useRef<HTMLDivElement>(null);
+
     return (
         <>
-            <div className="w-full dark:bg-slate-900 p-4 text-gray-700 dark:text-slate-100 font-semibold border-b border-gray-300 dark:border-gray-800">
-                <div className="flex items-center gap-4 justify-between">
-                    <div className="flex items-center gap-4">
+            <div className="relative w-full p-4 font-semibold text-gray-700 border-b border-gray-300 dark:bg-slate-900 dark:text-slate-100 dark:border-gray-800">
+                <div className="flex items-center justify-between gap-4">
+                    <div
+                        ref={triggerRef}
+                        onClick={() => setActiveDropdown(!activeDropdown)}
+                        className="flex items-center gap-4 cursor-pointer group">
                         <img
                             src={testProfilePicture}
                             alt="user profile picture"
-                            className="object-cover w-12 h-12 border rounded-full border-blue-400/30"
+                            className="object-cover w-12 h-12 border rounded-full border-blue-400/30 group-hover:bg-blue-500"
                         />
-                        <h4>Pessoa legal</h4>
+                        <h4 className="group-hover:text-blue-500">Pessoa legal</h4>
                     </div>
-                    <div className="flex gap-4">
-                        <Phone />
-                        <Settings />
+                    <div className="flex items-center gap-4">
+                        <Phone
+                            onClick={() => window.alert("this is not a feature for now, try again later")}
+                            className="cursor-pointer hover:text-blue-500"
+                        />
+                        <Video
+                            onClick={() => window.alert("this is not a feature for now, try again later")}
+                            className="cursor-pointer hover:text-blue-500"
+                        />
+                        <Search onClick={() => setActiveSearchBar(!activeSearchBar)} className="cursor-pointer hover:text-blue-500" />
                     </div>
+                </div>
+                <div className="relative">
+                    <ProfileDropdown
+                        activeDropdown={activeDropdown}
+                        displayName={"Pessoa legal"}
+                        profilePicture={testProfilePicture}
+                        username={"leandrolegal123"}
+                        triggerRef={triggerRef}
+                        setActiveDropdown={setActiveDropdown}
+                    />
+                    <SearchBar isSearchBarActive={activeSearchBar}/>
                 </div>
             </div>
         </>
