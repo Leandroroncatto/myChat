55ae7121c68280d1a78d403e9a3a281a9768b110
Leandro <leandroroncatto18@gmail.com>
2025-11-28 16:43:17 -0300

    feat: add language context

--- src/components/chat/EmptyChat.tsx
+++ src/components/chat/EmptyChat.tsx
@@ -1,13 +1,20 @@
 import { CloudFogIcon } from "lucide-react";
+import { useContext } from "react";
+import LanguageContext from "../../context/LanguageContext";
 
 export default function EmptyChat() {
+    const { languageData } = useContext(LanguageContext);
+
     return (
         <div className="flex items-center justify-center w-full h-full">
             <div className="flex flex-col items-center justify-center text-center">
                 <CloudFogIcon size={120} className="mb-4 text-blue-500" />
                 <h4 className="font-semibold text-gray-800 dark:text-slate-200">
-                    Select a conversation or start a{" "}
-                    <span className="font-bold text-blue-500 underline cursor-pointer">new one</span>
+                    {languageData.chatsSection.emptyChat.title}
+                    {" "}
+                    <button className="font-bold text-blue-500 underline cursor-pointer">
+                        {languageData.chatsSection.emptyChat.buttonText}
+                    </button>
                 </h4>
             </div>
         </div>
