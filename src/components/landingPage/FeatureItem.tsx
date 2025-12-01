56d046335ebf975ccf7998f6c4eb65efcf3530bd
Leandro <leandroroncatto18@gmail.com>
2025-12-01 19:05:16 -0300

    feat: create feature item component

---
+++ src/components/landingPage/FeatureItem.tsx
@@ -0,0 +1,19 @@
+export type featureItemTypes = {
+    icon: any;
+    title: string;
+    text: string;
+};
+
+export default function FeatureItem({ icon, title, text }: featureItemTypes) {
+    return (
+        <>
+            <div className="p-8 bg-white border border-gray-300 shadow-sm rounded-2xl">
+                <div className="flex flex-col gap-4">
+                    <div className="p-3 w-fit bg-blue-500/30 rounded-xl">{icon}</div>
+                    <h1 className="text-xl font-semibold text-gray-800">{title} </h1>
+                    <p className="text-gray-600">{text}</p>
+                </div>
+            </div>
+        </>
+    );
+}
