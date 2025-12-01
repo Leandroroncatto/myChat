ab7942d514662c8a14ba6552886c172cc6735fc4
Leandro <leandroroncatto18@gmail.com>
2025-12-01 19:04:57 -0300

    feat: create landing page footer

---
+++ src/components/landingPage/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="bg-slate-900">
+                <div className="container p-4 py-12 mx-auto">
+                    <div className="flex items-center justify-between">
+                        <p className="text-xl font-extrabold text-gray-200">myChat</p>
+                        <p className="text-xs font-semibold text-center text-gray-400 sm:text-sm">2025, myChat. Todos direitos reservados.</p>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+}
