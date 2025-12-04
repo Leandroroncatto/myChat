03c26b93ba3ff37652d3e9364ad67be3caffa7c7
Leandro <leandroroncatto18@gmail.com>
2025-12-04 17:53:40 -0300

    styles: made blue gradient stronger

--- src/components/auth/RightPanel.tsx
+++ src/components/auth/RightPanel.tsx
@@ -3,7 +3,7 @@ import type { LangData } from "../../types/globals/LangData";
 export function RightPanel({ languageData }: { languageData: LangData }) {
     return (
         <>
-            <div className="hidden lg:block lg:w-1/2 bg-linear-to-br from-blue-500/30 via-blue-500/50 to-blue-500/80">
+            <div className="hidden lg:block lg:w-1/2 bg-linear-to-br from-blue-500 via-blue-600/70 to-blue-500/60">
                 <div className="relative z-10 flex items-center justify-center h-full p-10 text-center text-white">
                     <div>
                         <h3 className="mb-4 text-4xl font-extrabold">{languageData.auth?.loginPage.sidebarTitle}</h3>
