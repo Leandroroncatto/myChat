e581d76a47fb36c82e50241f947ada953d6486c6
Leandro <leandroroncatto18@gmail.com>
2025-12-04 20:08:00 -0300

    refactor: replace inputs with new inputIcon component

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
 import type { LangData } from "../../types/globals/LangData";
 import { LockIcon, UserIcon } from "lucide-react";
+import InputIcon from "../ui/InputIcon";
 
 export default function LoginForm({ languageData }: { languageData: LangData }) {
     return (
@@ -18,26 +19,28 @@ export default function LoginForm({ languageData }: { languageData: LangData }) 
                     <label htmlFor="email" className="text-sm text-gray-800 dark:text-gray-200">
                         {languageData.auth?.loginPage.emailLabel}
                     </label>
-                    <input
+                    <InputIcon
                         type="email"
+                        Icon={UserIcon}
+                        className="p-3 pl-10 text-gray-900 placeholder-gray-400 transition duration-150 bg-white border border-gray-300 rounded-lg shadow-sm dark:border-gray-600 dark:bg-slate-800 dark:text-gray-100 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
                         id="email"
                         name="email"
-                        className="p-3 pl-10 text-gray-900 placeholder-gray-400 transition duration-150 bg-white border border-gray-300 rounded-lg shadow-sm dark:border-gray-600 dark:bg-slate-800 dark:text-gray-100 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
-                    <UserIcon className="absolute w-4 h-4 text-blue-500 top-10.5 left-4" />
                 </div>
                 <div className="relative flex flex-col gap-1">
                     <label htmlFor="password" className="text-sm text-gray-800 dark:text-gray-200">
                         {languageData.auth?.loginPage.passwordLabel}
                     </label>
-                    <input
+                    <InputIcon
                         type="password"
+                        Icon={LockIcon}
+                        className="p-3 pl-10 text-gray-900 placeholder-gray-400 transition duration-150 bg-white border border-gray-300 rounded-lg shadow-sm dark:border-gray-600 dark:bg-slate-800 dark:text-gray-100 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
                         id="password"
                         name="password"
-                        className="p-3 pl-10 text-gray-900 placeholder-gray-400 transition duration-150 bg-white border border-gray-300 rounded-lg shadow-sm dark:border-gray-600 dark:bg-slate-800 dark:text-gray-100 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
-                    <LockIcon className="absolute w-4 h-4 text-blue-500 top-10.5 left-4" />
-                    <Link to={"/forgot"} className="text-sm text-blue-500">{languageData.auth?.loginPage.forgotPassword}</Link>
+                    <Link to={"/forgot"} className="text-sm text-blue-500">
+                        {languageData.auth?.loginPage.forgotPassword}
+                    </Link>
                 </div>
                 <button className="p-4 font-semibold text-white bg-blue-500 cursor-pointer rounded-2xl hover:bg-blue-600">
                     {languageData.auth?.loginPage.signInButton}
